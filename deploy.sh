b121b83b39e789445f9e43353d774127f1e7ff3a
junseok yun <roadvirushn@gmail.com>
2022-10-16 20:18:58 +0900

    [TEST] push hook test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "Stop old version"
 docker-compose down
+echo "build new image"
 docker ps -a
 docker container prune -f
 docker image prune --all --force
+echo "deploy new version"
 docker-compose up -d
 echo "deploy done"
