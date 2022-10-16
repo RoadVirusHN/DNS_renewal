67c71f4673257a11e385cf831844205b64036682
junseok yun <roadvirushn@gmail.com>
2022-10-16 21:09:35 +0900

    [TEST] hook test

--- front-sk/src/http-common.js
+++ front-sk/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://13.125.223.239:8000/api",
   headers: {
     "Content-type": "application/json"
   }
