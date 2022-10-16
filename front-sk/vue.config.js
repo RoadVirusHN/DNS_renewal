6cce485d4c17b0974988085922719045fe5d955e
junseok yun <roadvirushn@gmail.com>
2022-10-16 23:43:33 +0900

    [TEST] http-common test

--- front-sk/vue.config.js
+++ front-sk/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
       host: '0.0.0.0',
       hot: true,
       port: '80',
-      public: "http://127.0.0.1:80/",
+      public: "http://54.180.8.164:80/",
       disableHostCheck: true
     }
   
