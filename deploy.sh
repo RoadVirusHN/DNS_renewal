dc6daf32824511f0c1dc9142f7d8bdc7209104ea
junseok yun <roadvirushn@gmail.com>
2022-10-16 14:00:00 +0900

    [Chore]Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/ec2-user/DNS_renewal
+pwd
+git pull origin main
+docker-compose down
+docker ps -a
+docker container prune -f
+docker image prune --all --force
+docker system prune
+docker-compose up
